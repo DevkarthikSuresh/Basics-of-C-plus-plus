1181709963be3addc73438657da1c42b7e169f29
DevkarthikSuresh <devkarthik300@gmail.com>
2024-07-29 13:22:27 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+   
+    float num1,num2;
+    cout << "Enter num1: ";
+    cin >> num1;
+    cout << "Enter num2: ";
+    cin >> num2;
+    cout << "The sum is: " << num1+num2 << "\n";;
+    cout << "The difference is: " << num1-num2 << "\n";;
+    cout << "The division is: " << num1/num2 << "\n";;
+    cout << "The product is: " << num1*num2 << "\n";;
+
+    return 0;
+}
