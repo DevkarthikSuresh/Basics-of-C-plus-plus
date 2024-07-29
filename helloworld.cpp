ea369a21f29be4ba5693e72156b8e80cbb7c09fe
DevkarthikSuresh <devkarthik300@gmail.com>
2024-07-29 13:19:48 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+//Name - Devarthik Suresh
+//PRN - 23070123045
+//Aim - Printing out Hellow World
+#include <iostream>
+using namespace std;
+int main() {
+    
+    cout << "Hello World";
+
+    return 0;
+}
+//Output- Hello World
